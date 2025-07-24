938f9a9a595f7370a9081c843ef5d08976dcf840
Kashish gupta <gkashish248@gmail.com>
2025-07-24 19:51:45 +0530

    Update square_and_sort_array.py

--- TWO POINTERS /square_and_sort_array.py
+++ TWO POINTERS /square_and_sort_array.py
@@ -4,8 +4,8 @@
 class Solution(object):
     def sortedSquares(self, nums):
 
-        sq_list = [num ** 2 for num in nums]
-        sq_list.sort()
+        sq_list = [num ** 2 for num in nums]    # O(N)
+        sq_list.sort()                        # O(N LOG N)
 
         return sq_list
     
